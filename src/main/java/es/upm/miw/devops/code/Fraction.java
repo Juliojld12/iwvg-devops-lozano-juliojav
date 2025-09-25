be187bea0858a04e772fedef01731abb297d21eb
Juliojld12 <119012467+Juliojld12@users.noreply.github.com>
2025-09-25 16:52:34 +0200

    add Fraction class and tests #4

---
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -0,0 +1,33 @@
+package es.upm.miw.devops.code;
+
+public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominator cannot be zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public double decimal() {
+        return (double) numerator / denominator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
+
